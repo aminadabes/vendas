b823d1479b4f0bc32fdecfcb3e50e32c5969bc91
Aminadabes Filho <amina.jr@hotmail.com>
2021-11-03 10:21:18 -0300

    Projetos iniciados e criados

---
+++ backend/src/main/java/com/amina/vsvendas/VsvendasApplication.java
@@ -0,0 +1,13 @@
+package com.amina.vsvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsvendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/amina/vsvendas/VsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amina.vsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
