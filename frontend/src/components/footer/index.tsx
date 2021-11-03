7e6890be877daf3d54a243c9b3c2a264bc772876
Aminadabes Filho <amina.jr@hotmail.com>
2021-11-03 15:14:00 -0300

    Retirada de texto do Footer

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -3,8 +3,7 @@ const Footer = () => {
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/aminadabes/vendas" target="_blank" rel="noreferrer">Aminadabes Filho</a></p>
-                <p className="text-light"><small><strong>Semana Spring React</strong><br/>
-            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                
             </div>
         </footer>
     );
